7969c4615ca1f56539eafe29b23db995626580cb
mini kk <kanbang@163.com>
2026-01-16 23:07:21 +0800

    okokoko

--- web/src/components/SchemaManager/SchemaList.vue
+++ web/src/components/SchemaManager/SchemaList.vue
@@ -70,8 +70,6 @@ function handleSelect(id: string) {
   width: 100%;
   height: 100%;
   background: #2d2d2d;
-  /* 边框由外部 Splitter 控制，这里也可以保留作为兜底 */
-  border-right: 1px solid #444; 
   display: flex;
   flex-direction: column;
   user-select: none;
